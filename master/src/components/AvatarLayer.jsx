4ccb2858a1b6ee460ef31cc7817b803e8d579820
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-12-09 21:19:40 +0600

    avatar add

--- master/src/components/AvatarLayer.jsx
+++ master/src/components/AvatarLayer.jsx
@@ -1,36 +1,32 @@
-import React from 'react'
-import AvatarSizes from './child/AvatarSizes'
-import AvatarWithContent from './child/AvatarWithContent'
-import AvatarShapeStyle from './child/AvatarShapeStyle'
-import StatusIndicator from './child/StatusIndicator'
-import AvatarGroup from './child/AvatarGroup'
-import ImagesWithContent from './child/ImagesWithContent'
+import AvatarSizes from "./child/AvatarSizes";
+import AvatarWithContent from "./child/AvatarWithContent";
+import AvatarShapeStyle from "./child/AvatarShapeStyle";
+import StatusIndicator from "./child/StatusIndicator";
+import AvatarGroup from "./child/AvatarGroup";
+import ImagesWithContent from "./child/ImagesWithContent";
 
 const AvatarLayer = () => {
-    return (
-        <div className="row gy-4">
-            {/* AvatarSizes */}
-            <AvatarSizes />
+  return (
+    <div className='row gy-4'>
+      {/* AvatarSizes */}
+      <AvatarSizes />
 
-            {/* AvatarWithContent */}
-            <AvatarWithContent />
+      {/* AvatarWithContent */}
+      <AvatarWithContent />
 
+      {/* AvatarShapeStyle */}
+      <AvatarShapeStyle />
 
-            {/* AvatarShapeStyle */}
-            <AvatarShapeStyle />
+      {/* StatusIndicator */}
+      <StatusIndicator />
 
-            {/* StatusIndicator */}
-            <StatusIndicator />
+      {/* AvatarGroup */}
+      <AvatarGroup />
 
-            {/* AvatarGroup */}
-            <AvatarGroup />
+      {/* ImagesWithContent */}
+      <ImagesWithContent />
+    </div>
+  );
+};
 
-            {/* ImagesWithContent */}
-            <ImagesWithContent />
-
-        </div>
-
-    )
-}
-
-export default AvatarLayer
+export default AvatarLayer;
