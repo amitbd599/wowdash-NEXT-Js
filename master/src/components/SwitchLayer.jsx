a43036ffd0564b4bc81dbef3960c57ea9fe1af7f
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-12-10 11:56:08 +0600

    switch add

--- master/src/components/SwitchLayer.jsx
+++ master/src/components/SwitchLayer.jsx
@@ -1,29 +1,24 @@
-import React from 'react'
-import DefaultRadioTwo from './child/DefaultRadioTwo'
-import SwitchDisable from './child/SwitchDisable'
-import SwitchWithTex from './child/SwitchWithTex'
-import SwitchHorizontal from './child/SwitchHorizontal'
+import DefaultRadioTwo from "./child/DefaultRadioTwo";
+import SwitchDisable from "./child/SwitchDisable";
+import SwitchWithTex from "./child/SwitchWithTex";
+import SwitchHorizontal from "./child/SwitchHorizontal";
 
 const SwitchLayer = () => {
-    return (
-        <div className="row gy-4">
+  return (
+    <div className='row gy-4'>
+      {/* DefaultRadioTwo */}
+      <DefaultRadioTwo />
 
-            {/* DefaultRadioTwo */}
-            <DefaultRadioTwo />
+      {/* SwitchDisable */}
+      <SwitchDisable />
 
+      {/* SwitchWithTex */}
+      <SwitchWithTex />
 
-            {/* SwitchDisable */}
-            <SwitchDisable />
+      {/* SwitchHorizontal */}
+      <SwitchHorizontal />
+    </div>
+  );
+};
 
-            {/* SwitchWithTex */}
-            <SwitchWithTex />
-
-            {/* SwitchHorizontal */}
-            <SwitchHorizontal />
-
-        </div>
-
-    )
-}
-
-export default SwitchLayer
+export default SwitchLayer;
