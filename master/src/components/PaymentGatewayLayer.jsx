694e5cec2b6eece4a6cc70a0417159bfdd04fe55
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-12-10 11:25:27 +0600

    payment-gateway add

--- master/src/components/PaymentGatewayLayer.jsx
+++ master/src/components/PaymentGatewayLayer.jsx
@@ -1,24 +1,20 @@
-import React from 'react'
-import Paypal from './child/Paypal'
-import RazorPay from './child/RazorPay'
+import Paypal from "./child/Paypal";
+import RazorPay from "./child/RazorPay";
 
 const PaymentGatewayLayer = () => {
-    return (
-        <div className="card h-100 p-0 radius-12">
-            <div className="card-body p-24">
-                <div className="row gy-4">
+  return (
+    <div className='card h-100 p-0 radius-12'>
+      <div className='card-body p-24'>
+        <div className='row gy-4'>
+          {/* Paypal */}
+          <Paypal />
 
-                    {/* Paypal */}
-                    <Paypal />
-
-                    {/* RazorPay */}
-                    <RazorPay />
-
-                </div>
-            </div>
+          {/* RazorPay */}
+          <RazorPay />
         </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default PaymentGatewayLayer
+export default PaymentGatewayLayer;
