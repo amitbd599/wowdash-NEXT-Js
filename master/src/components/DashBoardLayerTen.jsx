7345ff59925ae968504589996bae067293490096
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-12-31 17:08:54 +0600

    UnitCountSeven add

---
+++ master/src/components/DashBoardLayerTen.jsx
@@ -0,0 +1,33 @@
+import UnitCountSeven from "./child/UnitCountSeven";
+
+const DashBoardLayerTen = () => {
+  return (
+    <div className='row gy-4'>
+      {/* UnitCountSeven */}
+      <UnitCountSeven />
+
+      {/* IncomeVsExpense */}
+      {/* <IncomeVsExpense /> */}
+
+      {/* UsersChart */}
+      {/* <UsersChart /> */}
+
+      {/* TopSuppliers */}
+      {/* <TopSuppliers /> */}
+
+      {/* TopCustomer */}
+      {/* <TopCustomer /> */}
+
+      {/* OverallReport */}
+      {/* <OverallReport /> */}
+
+      {/* PurchaseAndSales */}
+      {/* <PurchaseAndSales /> */}
+
+      {/* RecentTransactions */}
+      {/* <RecentTransactions /> */}
+    </div>
+  );
+};
+
+export default DashBoardLayerTen;
